d7f2493fe71403eff796d19c39d7f466f24db543
Shubidumdu <skaty4870@naver.com>
2021-04-01 00:09:46 +0900

    Play with graphQL

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,146 @@
-import { ApolloServer, gql } from "apollo-server";
+import { ApolloServer, gql } from 'apollo-server';
 
 const typeDefs = gql`
-  type Book {
-    title: String
-    author: String
+  enum Score {
+    good
+    normal
+    bad
   }
 
   type Query {
-    books: [Book]
+    game(id: ID!): Game
+    developer(id: ID!): Developer
+  }
+
+  type Game {
+    id: ID!
+    title: String!
+    developer: Developer!
+    score: Score!
+  }
+
+  type Developer {
+    id: ID!
+    name: String!
+    games: [Game]!
   }
 `;
 
-const books = [
-  {
-    title: "The Awakening",
-    author: "Kate Chopin",
-  },
-  {
-    title: "City of Glass",
-    author: "Paul Auster",
-  },
-];
+interface Context {
+  db: DataBase;
+}
 
-const resolvers = {
-  Query: {
-    books: () => books,
+interface DataBase {
+  games: Game[];
+  developers: Developer[];
+}
+
+interface Game {
+  id: string;
+  title: string;
+  developer: string;
+  score: Score;
+}
+
+type Score = 'good' | 'normal' | 'bad';
+
+interface Developer {
+  id: string;
+  name: string;
+  games: string[];
+}
+
+// mocking db
+const context: Context = {
+  db: {
+    games: [
+      {
+        id: '1',
+        title: 'Super Mario Bros',
+        developer: '2',
+        score: 'good',
+      },
+      {
+        id: '2',
+        title: 'Tekken',
+        developer: '1',
+        score: 'good',
+      },
+      {
+        id: '3',
+        title: 'Pac-Man',
+        developer: '1',
+        score: 'bad',
+      },
+      {
+        id: '4',
+        title: 'PokeMon',
+        developer: '2',
+        score: 'normal',
+      },
+    ],
+    developers: [
+      {
+        id: '1',
+        name: 'Namco',
+        games: ['2', '3'],
+      },
+      {
+        id: '2',
+        name: 'Nintendo',
+        games: ['1', '4'],
+      },
+    ],
   },
 };
 
-const server = new ApolloServer({ typeDefs, resolvers });
+interface GameArgs {
+  id: string;
+}
+
+interface DeveloperArgs {
+  id: string;
+}
+
+const resolvers = {
+  Query: {
+    game: (_: any, { id }: GameArgs, context: Context) => {
+      return context.db.games.find(({ id: gameId }) => id === gameId);
+    },
+    developer: (_: any, { id }: DeveloperArgs, context: Context) => {
+      return context.db.developers.find(
+        ({ id: developerId }) => id === developerId,
+      );
+    },
+  },
+  Game: {
+    id: (game: Game) => game.id,
+    title: (game: Game) => game.title,
+    developer: ({ developer: id }: Game, _: any, context: Context) => {
+      return context.db.developers.find(
+        ({ id: developerId }) => id === developerId,
+      );
+    },
+    score: (game: Game) => {
+      return game.score;
+    },
+  },
+  Developer: {
+    id: (developer: Developer) => developer.id,
+    name: (developer: Developer) => developer.name,
+    games: ({ games }: Developer, _: any, context: Context) => {
+      return games.map((gameId) =>
+        context.db.games.find(({ id }) => id === gameId),
+      );
+    },
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context,
+});
 
 server.listen().then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
